5ddb9034da45c750b6622e32f05676261f56071a
unfairlife <93026350+unfairlife@users.noreply.github.com>
2021-10-23 02:52:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ starter = int(0)
 time.sleep(2)
 
 #loop 30min (add random time to avoid getting detected)
+# Set your Browser name here. Two examples are provided, chrome and opera(gx).
 def main():
     while keyboard.is_pressed('q') == False and starter == 0:
         w = WindowMgr()
@@ -21,17 +22,18 @@ def main():
         time.sleep(0.5)
         automation.automation()
         time.sleep(3)
+        #If the login page fails and the button "ok" is located, try to run again
         if pyautogui.locateOnScreen('ok_button.jpg', grayscale=True, confidence=0.8) != None:
             main()
-        w = WindowMgr()
-        w.find_window_wildcard("Bombcrypto - Opera")
-        w.set_foreground()
-        time.sleep(0.5)
-        automation.automation()
-        if pyautogui.locateOnScreen('ok_button.jpg', grayscale=True, confidence=0.8) != None:
-            main()
-        #Wait one hour to rerun and make everyone go to work again
-        time.sleep(randint(2400, 3000))
+       # w = WindowMgr()
+     #   w.find_window_wildcard("Bombcrypto - Opera")
+     #   w.set_foreground()
+      #  time.sleep(0.5)
+      #  automation.automation()
+      #  if pyautogui.locateOnScreen('ok_button.jpg', grayscale=True, confidence=0.8) != None:
+      #      main()
+      #  #Wait one hour to rerun and make everyone go to work again
+      #  time.sleep(randint(2400, 3000))
 main()
 
         
